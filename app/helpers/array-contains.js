7084f7875a702bab7c498250e5b10944ca73bcd7
Valby <21702419@etu.unicaen.fr>
2019-02-26 15:06:17 +0100

    reglage probleme git

--- app/helpers/array-contains.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function arrayContains(params/*, hash*/) {
-  const [items, value] = params;
-  return items.includes(value);
-}
-
-export default Ember.Helper.helper(arrayContains);
