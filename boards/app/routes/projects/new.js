4b8cdc473285b3cbf604d2e4d0b8bd8c51dd71a5
Valby <21702419@etu.unicaen.fr>
2019-04-06 18:36:23 +0200

    reglage problème liste deroulante

--- boards/app/routes/projects/new.js
+++ boards/app/routes/projects/new.js
@@ -5,12 +5,12 @@ export default Route.extend({
   model(){
     return Ember.RSVP.hash({
       developers: this.store.findAll("developer"),
+      copy: {},
     });
   },
   actions:{
     save(project){
       project = this.store.createRecord('project', project.copy);
-      console.log(project.get("name") + " " + project.get("owner"));
       project.save().then(this.transitionTo('projects'));
     },
     annuler(){
--- boards/app/routes/story/new.js
+++ boards/app/routes/story/new.js
@@ -2,26 +2,19 @@ import Route from '@ember/routing/route';
 import EmberObject, {get} from '@ember/object';
 
 export default Route.extend({
-  model(){
+  model(data){
     return Ember.RSVP.hash({
-      developers: this.store.findAll("developer")
+      developers: this.store.findAll("developer"),
+      copy: {project: this.get('store').findRecord('project',data.project_id)},
     });
   },
   actions: {
     save(story){
-      this.set("tags", null);
-      story = this.store.createRecord('story', story);
-      console.log(story.developer);
-      story.save();//.then(this.transitionTo('projects'));
+      story = this.store.createRecord('story', story.copy);
+      story.save().then(this.transitionTo('projects'));
     },
     annuler(){
       this.transitionTo('projects');
     },
-    /*didTransition() {
-      Ember.run.next(this, 'initUI');
-    },
-    initUI() {
-      Ember.$('.ui.dropdown').dropdown();
-    }*/
   }
 });
